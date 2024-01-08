340e2f3b204949444509bd25ebd4376666481174
antoni silva <ansir.manuel@gmail.com>
2024-01-08 00:21:54 -0500

    first commit

---
+++ src/app/guard/user-logoutguard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../demo/service/auth.service';
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class UserLogoutGuard implements CanActivate{
+    constructor(
+
+        private authService:AuthService,
+        private router:Router
+    ){}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        if(!this.authService.isLogued()){
+            return true;
+        }
+        this.router.navigate(["/"]);
+        return false;
+    }
+}
