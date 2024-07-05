b4569158bfb392007e78248679bdf81d2c942a78
antoni silva <ansir.manuel@gmail.com>
2024-07-05 10:12:14 -0500

    guard login

--- src/app/guard/user-logoutguard.ts
+++ src/app/guard/user-logoutguard.ts
@@ -1,23 +1,48 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    CanActivateFn,
+    Router,
+    RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from '../demo/service/auth.service';
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root',
 })
-
-export class UserLogoutGuard implements CanActivate{
-    constructor(
-
-        private authService:AuthService,
-        private router:Router
-    ){}
-
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        if(!this.authService.isLogued()){
+class GuardService {
+    constructor(private authService: AuthService, private router: Router) {}
+    CanActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): boolean {
+        if (!this.authService.isLogued()) {
             return true;
         }
-        this.router.navigate(["/"]);
+        this.router.navigate(['/']);
         return false;
     }
 }
+
+export const UserLogoutGuard: CanActivateFn = (
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+): boolean => {
+    return inject(GuardService).CanActivate(next, state);
+};
+
+// export class UserLogoutGuard implements CanActivate{
+//     constructor(
+
+//         private authService:AuthService,
+//         private router:Router
+//     ){}
+
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+//         if(!this.authService.isLogued()){
+//             return true;
+//         }
+//         this.router.navigate(["/"]);
+//         return false;
+//     }
+// }
